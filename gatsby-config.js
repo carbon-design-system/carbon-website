31601a710ee57be4c593657eca82f6271da64223
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-07-31 22:47:09 -0500

    chore: update site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,16 @@
 module.exports = {
   siteMetadata: {
     title: 'Carbon Design System',
-    description: 'A Gatsby theme for the carbon design system',
-    keywords: 'gatsby,theme,carbon',
+    description:
+      'Carbon is the design system for IBM web and product. It is a series of individual styles, components, and guidelines used for creating unified UI.',
+    keywords:
+      'IBM, design, system, Carbon, design system, Bluemix, styleguide, style, guide, components, library, pattern, kit, component, cloud',
   },
   plugins: [
     {
       resolve: 'gatsby-theme-carbon',
       options: {
         mdxExtensions: ['.mdx'],
-        imageQuality: 50,
         repository: {
           baseUrl: 'https://github.com/carbon-design-system/carbon-www',
           subDirectory: '',
