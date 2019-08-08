145599558747c3515611ece3a34784f952a22f26
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-08-08 16:39:01 -0500

    feat: add icon library component

--- src/components/IconLibrary/index.js
+++ src/components/IconLibrary/index.js
@@ -1,3 +1,76 @@
-import IconLibrary from './IconLibrary';
+/* eslint-disable no-debugger */
+import React, { useEffect, useState } from 'react';
+import { pickBy, keyBy } from 'lodash';
+import { icons as iconMetaData } from '../../data/metadata.json';
 
+const flattenedIconMetaData = iconMetaData.flatMap(
+  ({ variants, ...parent }) => {
+    // top level icon
+    if (!variants) return parent;
+
+    // give parent aliases to children
+    const variantsWithParentData = variants.map(variant => ({
+      aliases: parent.aliases,
+      ...variant,
+    }));
+
+    // don't return shell parents
+    if (parent.categories.length < 1) {
+      return variantsWithParentData;
+    }
+
+    // if parent is an icon, include it
+    return [parent, ...variantsWithParentData];
+  }
+);
+
+const IconLibrary = () => {
+  const [iconComponents, setIconComponents] = useState([]);
+  useEffect(() => {
+    import('@carbon/icons-react').then(iconsReact => {
+      const iconComponentList = pickBy(
+        iconsReact,
+        (val, key) => key.slice(-2) === '32'
+      );
+      const iconData = keyBy(flattenedIconMetaData, obj => {
+        const friendlyNameArr = obj.friendly_name.split(' ');
+        const moduleName = friendlyNameArr
+          .map(word => {
+            // if the 'word' is a number at the end, use the an underscore prefix
+            if (!isNaN(word)) {
+              return `_${word}`;
+            }
+            // capitalize each word before joining
+            return word[0].toUpperCase() + word.slice(1);
+          })
+          .join('');
+        return moduleName;
+      });
+
+      const iconArray = Object.keys(iconData).map(icon => ({
+        ...iconData[icon],
+        Component:
+          iconComponentList[`${icon}32`] ||
+          iconComponentList[`WatsonHealth${icon}32`] ||
+          iconComponentList[`Q${icon}32`],
+      }));
+
+      setIconComponents(iconArray);
+    });
+  }, []);
+
+  return (
+    <div>
+      {iconComponents.map(icon =>
+        icon.Component ? (
+          <icon.Component key={icon.name}>
+            <title>{icon.friendly_name}</title>
+          </icon.Component>
+        ) : (
+          <h1>error: {icon.name}</h1>
+        )
+      )}
+    </div>
+  );
+};
 export default IconLibrary;
