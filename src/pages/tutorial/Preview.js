be03af29829a2a445f96d0f69ad7fccf5afc4f6e
Alison Joseph <alisonejoseph@gmail.com>
2019-08-16 14:03:33 -0500

    chore: add missing image captions and sizing

--- src/pages/tutorial/Preview.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { preview } from './Preview.module.scss';
-
-const Preview = ({ title, ...props }) => (
-  <iframe title={title} {...props} className={preview} />
-);
-
-export default Preview;
