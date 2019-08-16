0546ff50a6c51b588d4250afc1ae15551cf0b35b
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-08-15 21:23:56 -0500

    fix: ssr

--- src/gatsby-theme-carbon/util/hooks/useSearch.js
+++ src/gatsby-theme-carbon/util/hooks/useSearch.js
@@ -1,16 +1,22 @@
 import { useLayoutEffect } from 'react';
 import 'docsearch.js/dist/cdn/docsearch.min.css';
-import docsearch from 'docsearch.js';
 
 const useAlgoliaSearch = () => {
   useLayoutEffect(() => {
-    docsearch({
-      apiKey: `296ea0c1d0e96b2b04900d0f4d1a6329`,
-      indexName: `carbondesignsystem`,
-      inputSelector: `#doc-search`,
-      // set this to true if you need to debug css
-      debug: false,
-    });
+    if (
+      typeof window === `undefined` ||
+      typeof window.docsearch === `undefined`
+    ) {
+      console.warn(`Search has failed to load and now is being disabled`);
+    } else {
+      window.docsearch({
+        apiKey: `296ea0c1d0e96b2b04900d0f4d1a6329`,
+        indexName: `carbondesignsystem`,
+        inputSelector: `#doc-search`,
+        // set this to true if you need to debug css
+        debug: false,
+      });
+    }
   });
 };
 
