e5126dfc3afb6527425c299d9e6a09b1fbb09cc2
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-08-19 15:33:06 -0700

    fix: chrome

--- src/gatsby-theme-carbon/util/hooks/useSearch.js
+++ src/gatsby-theme-carbon/util/hooks/useSearch.js
@@ -1,5 +1,4 @@
 import { useLayoutEffect } from 'react';
-import 'docsearch.js/dist/cdn/docsearch.min.css';
 
 const useAlgoliaSearch = () => {
   useLayoutEffect(() => {
@@ -13,11 +12,10 @@ const useAlgoliaSearch = () => {
         apiKey: `296ea0c1d0e96b2b04900d0f4d1a6329`,
         indexName: `carbondesignsystem`,
         inputSelector: `#doc-search`,
-        // set this to true if you need to debug css
         debug: false,
       });
     }
-  });
+  }, [window.docsearch]);
 };
 
 export default useAlgoliaSearch;
