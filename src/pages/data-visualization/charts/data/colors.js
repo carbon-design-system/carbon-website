265d71164186fa33ea58447a2288add07e030f8b
Alison Joseph <alisonejoseph@gmail.com>
2019-09-11 08:32:23 -0500

    feat: import charts directly to mdx

---
+++ src/pages/data-visualization/charts/data/colors.js
@@ -0,0 +1,8 @@
+export const colors = [
+  '#00a68f',
+  '#3b1a40',
+  '#473793',
+  '#3c6df0',
+  '#56D2BB',
+  // 12 items needed
+];
--- src/pages/data-visualization/charts/data/demo-data-bar.js
+++ src/pages/data-visualization/charts/data/demo-data-bar.js
@@ -1,11 +1,4 @@
-export const colors = [
-  '#00a68f',
-  '#3b1a40',
-  '#473793',
-  '#3c6df0',
-  '#56D2BB',
-  // 12 items needed
-];
+import { colors } from './colors';
 
 export const groupedBarData = {
   labels: ['Qty', 'More', 'Sold', 'Restocking', 'Misc'],
