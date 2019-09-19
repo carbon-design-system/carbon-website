467015a74a10745a6a032767e901f66aa8ce0404
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-09-19 15:42:54 -0500

    fix: broken get-started-link, make react first tab

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,7 @@ import { FourOhFour } from 'gatsby-theme-carbon';
 const links = [
   { href: '/get-started/about-carbon', text: 'About Carbon' },
   { href: '/components/overview', text: 'Components overview' },
-  { href: '/get-started/development', text: 'Get started: development' },
+  { href: '/get-started/development/react', text: 'Get started: development' },
   { href: '/get-started/design', text: 'Get started: design' },
 ];
 
