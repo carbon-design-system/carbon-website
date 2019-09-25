7750e2fff1ee22a61d883c28c2916d7d6f068c86
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-09-25 18:20:22 -0500

    chore: add pictogram tabs and rename iconography to icons

--- src/pages/guidelines/icons/library.js
+++ src/pages/guidelines/icons/library.js
@@ -5,10 +5,10 @@ import IconLibrary from '../../../components/SvgLibraries/IconLibrary';
 const frontmatter = {
   label:
     'Icons are visual symbols used to represent ideas, objects, or actions. They communicate messages at a glance, afford interactivity, and draw attention to important information.',
-  title: 'Iconography',
+  title: 'Icons',
   description:
     'Icons are visual symbols used to represent ideas, objects, or actions. They communicate messages at a glance, afford interactivity, and draw attention to important information.',
-  tabs: ['Usage', 'Icons', 'Pictograms', 'Contribute'],
+  tabs: ['Library', 'Usage', 'Contribute'],
 };
 
 export default ({ pageContext, ...rest }) => (
