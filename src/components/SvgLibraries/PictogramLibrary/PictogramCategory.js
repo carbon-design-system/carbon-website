2e645d0b628273ae13f79da17a066af4626506d2
CHRISTOPHER M. CONNORS <connorsc@us.ibm.com>
2019-09-27 16:16:42 -0400

    Update PictogramCategory.js
    removes scaling attribute since desired display size is native size

--- src/components/SvgLibraries/PictogramLibrary/PictogramCategory.js
+++ src/components/SvgLibraries/PictogramLibrary/PictogramCategory.js
@@ -16,7 +16,7 @@ const IconCategory = ({ category, pictograms }) => (
     <h2 className={cx(h2, categoryTitle)}>{category}</h2>
     <ul className={svgGrid}>
       {pictograms.map(pictogram => (
-        <SvgCard key={pictogram.name} icon={pictogram} height={48} width={48} />
+        <SvgCard key={pictogram.name} icon={pictogram} />
       ))}
     </ul>
   </section>
