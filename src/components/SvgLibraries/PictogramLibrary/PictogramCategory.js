e135cd9e52850a9fc54b7aa0ba4dc8d70181b759
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-10-01 09:57:40 -0500

    fix: add classnames and larger mobile size

--- src/components/SvgLibraries/PictogramLibrary/PictogramCategory.js
+++ src/components/SvgLibraries/PictogramLibrary/PictogramCategory.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { h2 } from 'gatsby-theme-carbon/src/components/markdown/Markdown.module.scss';
 import cx from 'classnames';
+import useMedia from 'use-media';
 
 import SvgCard from '../shared/SvgCard';
 
@@ -9,17 +10,27 @@ import {
   svgGrid,
   categoryTitle,
   svgCategory,
+  pictograms as pictogramList,
 } from '../shared/SvgLibrary.module.scss';
 
-const IconCategory = ({ category, pictograms }) => (
-  <section className={svgCategory}>
-    <h2 className={cx(h2, categoryTitle)}>{category}</h2>
-    <ul className={svgGrid}>
-      {pictograms.map(pictogram => (
-        <SvgCard key={pictogram.name} icon={pictogram} height={98} width={98} />
-      ))}
-    </ul>
-  </section>
-);
+const IconCategory = ({ category, pictograms }) => {
+  const isMobile = useMedia({ maxWidth: 1000 });
+  const size = isMobile ? 156 : 128;
+  return (
+    <section className={svgCategory}>
+      <h2 className={cx(h2, categoryTitle)}>{category}</h2>
+      <ul className={cx(svgGrid, pictogramList)}>
+        {pictograms.map(pictogram => (
+          <SvgCard
+            key={pictogram.name}
+            icon={pictogram}
+            height={size}
+            width={size}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+};
 
 export default IconCategory;
