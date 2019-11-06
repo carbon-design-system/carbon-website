5eed71bf6c9c8fbf4dc6f5df92d762221f3457bd
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-11-05 23:32:27 -0600

    feat: add enum/boolean input generation

--- src/components/ComponentDemo/KnobContainer.js
+++ src/components/ComponentDemo/KnobContainer.js
@@ -1,9 +1,10 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
 import {
   Form,
   FormGroup,
-  // RadioButton,
-  // RadioButtonGroup,
+  RadioButton,
+  RadioButtonGroup,
   Checkbox,
 } from 'carbon-components-react';
 import carbonReactDocgen from '../../data/react-docgen.json';
@@ -30,20 +31,59 @@ const ComponentKnobGroup = ({ component, knobs, ...rest }) => (
   </div>
 );
 
-const Knob = ({ name, info, inputId }) => {
+const Knob = ({ name, info, inputId, key }) => {
   const { type, description, defaultValue } = info;
+
   if (type.name === 'bool') {
+    const defaultChecked =
+      (defaultValue && defaultValue.value !== 'false') || undefined;
     return (
       <Checkbox
+        key={key}
         title={description}
-        defaultChecked={defaultValue && defaultValue.value}
+        defaultChecked={defaultChecked}
         labelText={name}
         className={knobFormItem}
         id={inputId}
       />
     );
   }
+
+  if (type.name === 'enum') {
+    const values = type.value;
+
+    let defaultSelected =
+      (defaultValue && defaultValue.value !== 'false') || undefined;
+
+    // some props (Button size) declare a default prop-type with no effect
+    if (values.map(({ value }) => value).includes(`'default'`)) {
+      defaultSelected = `'default'`;
+    }
+
+    return (
+      <FormGroup legendText={name}>
+        <RadioButtonGroup
+          defaultSelected={defaultSelected}
+          orientation="vertical">
+          {values.map(({ value }) => (
+            <RadioButton value={value} labelText={value.replace(/'/g, '')} />
+          ))}
+        </RadioButtonGroup>
+      </FormGroup>
+    );
+  }
   return '';
+};
+
+Knob.propTypes = {
+  info: (props, propName) => {
+    if (!/(enum|bool)/.test(props[propName].type.name)) {
+      return new Error(
+        `Knob '${props[propName]}' does not have a valid prop-type. Use a knob with an enum or boolean value.`
+      );
+    }
+    return null;
+  },
 };
 
 const KnobContainer = ({ knobs }) => {
