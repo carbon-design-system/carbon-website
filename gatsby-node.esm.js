a452c8ee187a7b281d077c955a334b8ddfc73bdf
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-11-12 12:46:12 -0600

    chore: use upstream-generated docgen

--- gatsby-node.esm.js
+++ gatsby-node.esm.js
@@ -4,11 +4,11 @@
 /* eslint-disable import/no-dynamic-require */
 /* eslint-disable import/no-unresolved */
 const path = require('path');
-const { execSync } = require('child_process');
-const fs = require('fs');
-const git = require('simple-git/promise');
+// const { execSync } = require('child_process');
+// const fs = require('fs');
+// const git = require('simple-git/promise');
 
-const rimraf = require('rimraf');
+// const rimraf = require('rimraf');
 
 exports.onCreateWebpackConfig = ({ actions }) => {
   // Allows importing html files for component code examples
@@ -36,37 +36,31 @@ exports.onCreateWebpackConfig = ({ actions }) => {
 
 // Generate docgen info from carbon-components-react
 // This could probably be a github action once the inital demos are written
+// Temporarily disabled in favor of commited data. Pending upstream babel gen or refinements.
+// https://github.com/carbon-design-system/carbon/pull/4662
 exports.onPreBootstrap = async () => {
-  const { sep } = path;
-  const tmpDir = fs.mkdtempSync(`.cache${sep}`);
-
-  await git()
-    .silent(true)
-    .clone(
-      'https://github.com/carbon-design-system/carbon.git',
-      `${tmpDir}/carbon`
-    );
-
-  const ccrPath = `${tmpDir}/carbon/packages/react/src`;
-
-  execSync(
-    `babel ./${ccrPath} --out-dir ${tmpDir} --config-file ./docgen-babel.config.js`
-  );
-
-  const CarbonComponentsReact = require(`./${tmpDir}`);
-
-  const components = Object.keys(CarbonComponentsReact);
-  const docgen = {};
-
-  components.forEach(component => {
-    // eslint-disable-next-line no-underscore-dangle
-    docgen[component] = CarbonComponentsReact[component].__docgenInfo;
-  });
-
-  fs.writeFileSync(
-    'src/data/docgen/react-docgen.json',
-    JSON.stringify(docgen, null, 2)
-  );
-
-  rimraf.sync(tmpDir);
+  // const { sep } = path;
+  // const tmpDir = fs.mkdtempSync(`.cache${sep}`);
+  // await git()
+  //   .silent(true)
+  //   .clone(
+  //     'https://github.com/carbon-design-system/carbon.git',
+  //     `${tmpDir}/carbon`
+  //   );
+  // const ccrPath = `${tmpDir}/carbon/packages/react/src`;
+  // execSync(
+  //   `babel ./${ccrPath} --out-dir ${tmpDir} --config-file ./docgen-babel.config.js`
+  // );
+  // const CarbonComponentsReact = require(`./${tmpDir}`);
+  // const components = Object.keys(CarbonComponentsReact);
+  // const docgen = {};
+  // components.forEach(component => {
+  //   // eslint-disable-next-line no-underscore-dangle
+  //   docgen[component] = CarbonComponentsReact[component].__docgenInfo;
+  // });
+  // fs.writeFileSync(
+  //   'src/data/docgen/react-docgen.json',
+  //   JSON.stringify(docgen, null, 2)
+  // );
+  // rimraf.sync(tmpDir);
 };
