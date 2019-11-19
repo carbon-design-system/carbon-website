e81a1f96eb84c850a403028f10e825a8267593dc
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-11-19 13:03:50 -0600

    chore: remove unnecessary build step and deps

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,25 @@
-const req = require('esm')(module);
+const path = require('path');
 
-module.exports = req('./gatsby-node.esm.js');
+exports.onCreateWebpackConfig = ({ actions }) => {
+  // Allows importing html files for component code examples
+  actions.setWebpackConfig({
+    module: {
+      rules: [
+        {
+          test: /\.md$/,
+          loaders: ['html-loader', 'markdown-loader'],
+        },
+        {
+          test: /\.html$/,
+          loader: 'html-loader',
+          options: {
+            minimize: false,
+          },
+        },
+      ],
+    },
+    resolve: {
+      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    },
+  });
+};
