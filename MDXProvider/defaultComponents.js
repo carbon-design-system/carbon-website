eb788ed0ebce9c71115e9fe3924134faa0bfe046
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-12-06 00:59:19 +0100

    fix: try no default components

---
+++ MDXProvider/defaultComponents.js
@@ -0,0 +1,8 @@
+// import defaultComponents from 'gatsby-theme-carbon/src/components/MDXProvider/defaultComponents';
+
+// import MdxIcon from '../src/components/MdxIcon';
+
+// export default {
+//   ...defaultComponents,
+//   MdxIcon,
+// };
