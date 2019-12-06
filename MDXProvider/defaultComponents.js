17d4a29746c25f410f54058eced63f6c27dabda7
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-12-07 00:24:46 +0100

    fix: remove icon/pictogram pages

--- MDXProvider/defaultComponents.js
+++
@@ -1,8 +0,0 @@
-// import defaultComponents from 'gatsby-theme-carbon/src/components/MDXProvider/defaultComponents';
-
-// import MdxIcon from '../src/components/MdxIcon';
-
-// export default {
-//   ...defaultComponents,
-//   MdxIcon,
-// };
