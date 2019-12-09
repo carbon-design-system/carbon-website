659aa0351787b4ce19177c09f21d4085721bc56c
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-12-09 15:19:19 -0600

    fix: don't use global provider

--- src/gatsby-theme-carbon/components/MDXProvider/defaultComponents.js
+++ src/gatsby-theme-carbon/components/MDXProvider/defaultComponents.js
@@ -1,10 +1,7 @@
 import defaultComponents from 'gatsby-theme-carbon/src/components/MDXProvider/defaultComponents';
-
 import MdxIcon from '../../../components/MdxIcon';
-import ComponentDemo from '../../../components/ComponentDemo';
 
 export default {
   ...defaultComponents,
-  ComponentDemo,
   MdxIcon,
 };
