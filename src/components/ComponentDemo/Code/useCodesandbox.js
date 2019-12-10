263db530439de383e2a6cc17e50d41e74b518873
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-12-10 09:16:16 -0600

    fix: remove standalone from bundle, run formatting

--- src/components/ComponentDemo/Code/useCodesandbox.js
+++ src/components/ComponentDemo/Code/useCodesandbox.js
@@ -1,12 +1,10 @@
 import { useMemo, useRef } from 'react';
 import { getParameters } from 'codesandbox/lib/api/define';
-import prettier from 'prettier/standalone';
-import parserBabylon from 'prettier/parser-babylon';
 
 const getIndex = ({ code = '' }) => {
-  const uniqueComponents = Array.from(new Set(code.match(/<[A-Z]\w+/g))).map(
-    component => component.slice(1)
-  );
+  const uniqueComponents = Array.from(
+    new Set(code.match(/<[A-Z]\w+/g))
+  ).map(component => component.slice(1));
 
   return `
       import React from 'react';
@@ -25,16 +23,7 @@ const getIndex = ({ code = '' }) => {
 const useCodesandbox = code => {
   const { current: originalCode } = useRef(code);
   const url = useMemo(() => {
-    let indexContent = getIndex({ code: originalCode });
-
-    try {
-      indexContent = prettier.format(getIndex({ code }), {
-        parser: 'babylon',
-        plugins: [parserBabylon],
-      });
-    } catch (e) {
-      console.warn(`Prettier error: ${e.message}`);
-    }
+    const indexContent = getIndex({ code: originalCode });
 
     const parameters = getParameters({
       files: {
