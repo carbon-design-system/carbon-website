bd3ec295cc7e4732825be38008ce5fdaf321342b
Simon Finney <simonjfinney@gmail.com>
2020-01-15 12:07:47 +0000

    chore(footer): remove 'Netlify' link

--- src/gatsby-theme-carbon/components/Footer.js
+++ src/gatsby-theme-carbon/components/Footer.js
@@ -43,7 +43,6 @@ const links = {
   secondCol: [
     { href: 'https://medium.com/carbondesign', linkText: 'Medium' },
     { href: 'https://twitter.com/_carbondesign', linkText: 'Twitter' },
-    { href: 'https://www.netlify.com/', linkText: 'This site is powered by Netlify' },
   ],
 };
 
