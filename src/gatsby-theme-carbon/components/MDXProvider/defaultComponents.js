7328b1caa9ae69bcfff1bfeb09bac591c11c4925
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2020-02-03 22:16:32 -0600

    chore: remove vanilla code stuff

--- src/gatsby-theme-carbon/components/MDXProvider/defaultComponents.js
+++ src/gatsby-theme-carbon/components/MDXProvider/defaultComponents.js
@@ -1,16 +1,13 @@
 import defaultComponents from 'gatsby-theme-carbon/src/components/MDXProvider/defaultComponents';
 
-import ComponentDocs from '../../../components/ComponentDocs';
-import ComponentCode from '../../../components/ComponentCode';
-import ComponentReact from '../../../components/ComponentReact';
+// import ComponentDocs from '../../../components/ComponentDocs';
+// import ComponentCode from '../../../components/ComponentCode';
+// import ComponentReact from '../../../components/ComponentReact';
 import MdxIcon from '../../../components/MdxIcon';
 import ComponentDemo from '../../../components/ComponentDemo';
 
 export default {
   ...defaultComponents,
-  ComponentDocs,
-  ComponentCode,
-  ComponentReact,
   ComponentDemo,
   MdxIcon,
 };
