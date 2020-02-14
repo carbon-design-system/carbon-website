b6cccdc4053756c13a1faa0eb18a198ed71b38a2
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2020-02-13 23:30:16 -0600

    fix: add prefix

--- src/components/SvgLibraries/shared/ActionBar.js
+++ src/components/SvgLibraries/shared/ActionBar.js
@@ -3,6 +3,7 @@ import { pascal } from 'change-case';
 import { Code16, Download16 } from '@carbon/icons-react';
 import { Copy } from 'carbon-components-react';
 import copy from 'copy-to-clipboard';
+import { withPrefix } from 'gatsby';
 import { LibraryContext } from './LibraryProvider';
 import styles from './ActionBar.module.scss';
 
@@ -35,7 +36,7 @@ const ActionBar = ({
       <a
         onFocus={() => setIsActionBarVisible(true)}
         download={`${name}.svg`}
-        href={`/${type}s/${name}.svg`}>
+        href={withPrefix(`/${type}s/${name}.svg`)}>
         <Download16 title={`download ${name}.svg`}>
           <title>Download {name}.svg</title>
         </Download16>
