413a3f7ace3eabada6a06d48166c16b5fd773e25
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2020-02-18 13:17:33 -0600

    chore: add reload

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,7 @@
+/* eslint-disable import/prefer-default-export */
 import './src/styles/index.scss';
+
+export const onServiceWorkerUpdateReady = () => {
+  window.location.reload();
+  console.log('sw updated');
+};
