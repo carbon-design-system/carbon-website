7725f8f157f46023a1c925d24656628cc1b24804
Vince Picone <vpicone@gmail.com>
2020-03-06 15:28:39 -0600

    feat:  track user languages (#990)
    * feat:  track top five languages
    
    * fix: ensure fathom has loaded
    
    Co-authored-by: Vince Picone <Vincent.Patrick.Picone@ibm.com>

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,30 @@
+/* eslint-disable import/prefer-default-export */
 import './src/styles/index.scss';
+
+export const onClientEntry = () => {
+  const trackLanguage = code => {
+    fathom('trackGoal', code, 0);
+  };
+
+  window.addEventListener('load', () => {
+    switch (window.navigator.language.slice(0, 2)) {
+      case 'en':
+        trackLanguage('U8CTWM12');
+        break;
+      case 'zh':
+        trackLanguage('U2CWMKXS');
+        break;
+      case 'es':
+        trackLanguage('KWIV8NQH');
+        break;
+      case 'ar':
+        trackLanguage('GM8KBYWH');
+        break;
+      case 'pt':
+        trackLanguage('2PYNY8FF');
+        break;
+      default:
+        trackLanguage('XBZNHQHJ');
+    }
+  });
+};
