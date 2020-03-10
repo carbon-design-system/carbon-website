3f4bf6e634a3dfbc3bd363a9e9fa2ba837cab9e1
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2020-03-09 21:43:03 -0500

    feat: add survey netlify function

---
+++ functions/survey.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const fetch = require('node-fetch');
+
+exports.handler = async function survey(event, _, callback) {
+  const { experience, comment, path } = JSON.parse(event.body);
+
+  const body = JSON.stringify({
+    data: {
+      '2': { value: experience },
+      '3': { value: comment },
+      '4': { value: path },
+    },
+  });
+
+  const res = await fetch(
+    `https://restapi.surveygizmo.com/v5/survey/${process.env.SURVEY_ID}/surveyresponse?api_token=${process.env.API_TOKEN}&api_token_secret=${process.env.API_TOKEN_SECRET}`,
+    {
+      method: 'PUT',
+      body,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  ).catch(error =>
+    callback(error.message, {
+      statusCode: error.code,
+    })
+  );
+
+  const json = await res.json();
+
+  callback(null, {
+    statusCode: 200,
+    body: json,
+  });
+};
