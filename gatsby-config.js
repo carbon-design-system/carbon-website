ef275c44e8c80b929559af07cb270e010d38519a
Vince Picone <vpicone@gmail.com>
2020-03-09 23:42:26 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,7 @@ module.exports = {
         isSearchEnabled: true,
         iconPath: './src/images/favicon.svg',
         titleType: 'prepend',
+        pngCompressionSpeed: 10,
         repository: {
           baseUrl: 'https://github.com/carbon-design-system/carbon-website',
           subDirectory: '',
