78b6f5c8c81d6664b833045b7fa121628a45d680
Vince Picone <vpicone@gmail.com>
2020-03-11 22:57:08 -0500

    feat: add auth (#1007)
    Co-authored-by: Vince Picone <Vincent.Patrick.Picone@ibm.com>

--- functions/survey.js
+++ functions/survey.js
@@ -1,7 +1,29 @@
 require('dotenv').config();
 const fetch = require('node-fetch');
+const cookie = require('cookie');
 
-exports.handler = async function survey(event, _, callback) {
+// const permittedOrigins = ['https://www.carbondesignsystem.com'];
+
+exports.handler = async function survey(event) {
+  // const { httpMethod, origin } = event;
+  if (event.headers.cookie) {
+    const { SURVEY_RECENTLY_SUBMITTED } = cookie.parse(event.headers.cookie);
+    if (SURVEY_RECENTLY_SUBMITTED) {
+      return {
+        statusCode: 429,
+        body: 'Survey recently submitted. Try again in 2 minutes.',
+      };
+    }
+  }
+
+  // if (httpMethod !== 'POST') {
+  //   return { statusCode: 405, body: 'Method Not Allowed' };
+  // }
+
+  // if (!permittedOrigins.includes(origin)) {
+  //   return { statusCode: 403, body: 'Invalid origin' };
+  // }
+
   const { experience, comment, path } = JSON.parse(event.body);
 
   const body = JSON.stringify({
@@ -12,25 +34,25 @@ exports.handler = async function survey(event, _, callback) {
     },
   });
 
-  const res = await fetch(
-    `https://restapi.surveygizmo.com/v5/survey/${process.env.SURVEY_ID}/surveyresponse?api_token=${process.env.SURVEYGIZMO_API_TOKEN}&api_token_secret=${process.env.SURVEYGIZMO_API_TOKEN_SECRET}`,
-    {
-      method: 'PUT',
-      body,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }
-  ).catch(error =>
-    callback(error.message, {
-      statusCode: error.code,
-    })
-  );
-
-  const json = await res.json();
-
-  callback(null, {
-    statusCode: 200,
-    body: json,
+  await fetch(process.env.SURVEYGIZMO_REQUEST_URI, {
+    method: 'PUT',
+    body,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).catch(({ statusCode, message }) => {
+    return { statusCode, body: message };
   });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify('Feedback submitted! Thank you.'),
+    headers: {
+      'Set-Cookie': `SURVEY_RECENTLY_SUBMITTED=true`,
+      'Max-Age': 120,
+      Secure: true,
+      SameSite: 'none',
+      HttpOnly: true,
+    },
+  };
 };
