393afbb3b1f0d26dc493fb619dea4744441a449f
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2020-03-16 12:54:47 -0500

    feat: add feedback dialog

---
+++ src/gatsby-theme-carbon/components/FeedbackDialog/FeedbackDialog.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ThemeFeedbackDialog from 'gatsby-theme-carbon/src/components/FeedbackDialog/FeedbackDialog';
+
+const FeedbackDialog = ({ props }) => {
+  const onSubmit = data => {
+    fetch(process.env.SURVEYGIZMO_REQUEST_URI, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      credentials: 'include',
+      mode: 'no-cors',
+    });
+  };
+
+  return <ThemeFeedbackDialog {...props} onSubmit={onSubmit} />;
+};
+
+export default FeedbackDialog;
