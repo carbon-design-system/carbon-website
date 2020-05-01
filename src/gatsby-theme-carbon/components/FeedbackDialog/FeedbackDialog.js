4e715938c7869c5609803b0e64dea6e7f7dc7b60
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2020-05-01 13:56:31 -0500

    feat: initial commit

--- src/gatsby-theme-carbon/components/FeedbackDialog/FeedbackDialog.js
+++ src/gatsby-theme-carbon/components/FeedbackDialog/FeedbackDialog.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ThemeFeedbackDialog from 'gatsby-theme-carbon/src/components/FeedbackDialog/FeedbackDialog';
 
 const FeedbackDialog = ({ props }) => {
-  const onSubmit = (data) => {
-    fetch('https://carbon-website.netlify.com/.netlify/functions/survey', {
+  const onSubmit = async (data) => {
+    const res = await fetch('/api/survey', {
       method: 'POST',
       body: JSON.stringify(data),
       credentials: 'include',
       mode: 'no-cors',
     });
+    const result = await res.json();
+    console.log(JSON.stringify(result, null, 2));
   };
 
   return <ThemeFeedbackDialog {...props} onSubmit={onSubmit} />;
