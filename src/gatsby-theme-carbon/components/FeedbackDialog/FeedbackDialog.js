89b3e80e95b33c4e5abc2a8feb2b7b5aed72c50b
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2020-05-01 21:04:56 -0500

    fix: remove console log

--- src/gatsby-theme-carbon/components/FeedbackDialog/FeedbackDialog.js
+++ src/gatsby-theme-carbon/components/FeedbackDialog/FeedbackDialog.js
@@ -3,14 +3,12 @@ import ThemeFeedbackDialog from 'gatsby-theme-carbon/src/components/FeedbackDial
 
 const FeedbackDialog = ({ props }) => {
   const onSubmit = async (data) => {
-    const res = await fetch('https://carbondesignsystem.now.sh/api/survey', {
+    fetch('https://carbondesignsystem.now.sh/api/survey', {
       method: 'POST',
       body: JSON.stringify(data),
       credentials: 'include',
       mode: 'no-cors',
     });
-    const result = await res.json();
-    console.log(JSON.stringify(result, null, 2));
   };
 
   return <ThemeFeedbackDialog {...props} onSubmit={onSubmit} />;
