4242744b63063fe9f801ade598fa7a47bed7441b
Vince Picone <vpicone@gmail.com>
2020-06-08 16:23:32 -0500

    fix: more-agressively pre-optimize images (#1260)
    Co-authored-by: Vince Picone <Vincent.Patrick.Picone@ibm.com>
    Co-authored-by: TJ Egan <tw15egan@gmail.com>

---
+++ scripts/optimize-images.js
@@ -0,0 +1,30 @@
+const sharp = require(`sharp`);
+const glob = require(`glob`);
+const fs = require(`fs-extra`);
+
+const matches = glob.sync(`src/**/*.{png,jpg,jpeg}`);
+const MAX_WIDTH = 1152;
+const QUALITY = 70;
+
+Promise.all(
+  matches.map(async (match) => {
+    const stream = sharp(match);
+    const info = await stream.metadata();
+
+    if (info.width < MAX_WIDTH) {
+      return;
+    }
+
+    const optimizedName = match.replace(
+      /(\..+)$/,
+      (_, ext) => `-optimized${ext}`
+    );
+
+    await stream
+      .resize(MAX_WIDTH)
+      .jpeg({ quality: QUALITY })
+      .toFile(optimizedName);
+
+    return fs.rename(optimizedName, match);
+  })
+);
