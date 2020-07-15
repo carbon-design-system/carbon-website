7ce2cd8450b9ab3c06d4b4fab5e8c2d140c8696b
andrea.cardonaibm.com <andrea.cardonaibm.com>
2020-07-15 11:12:35 -0400

    docs(colors: update gray 10 color on docs

--- src/data/guidelines/color-tokens.js
+++ src/data/guidelines/color-tokens.js
@@ -278,8 +278,8 @@ const colorTokens = {
           hex: '#393939',
         },
         g10: {
-          name: 'Gray 80',
-          hex: '#393939',
+          name: 'Gray 70',
+          hex: '#525252',
         },
         g90: {
           name: 'Gray 30',
