ee7d77ec18dbca09d0738e68106ba1cb673b87e9
Josefina Mancilla <josefinanoemi4@gmail.com>
2020-07-27 17:10:38 -0500

    feat: add copy functionality

--- src/components/ColorPalette/ColorPaletteColor.js
+++ src/components/ColorPalette/ColorPaletteColor.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import cx from 'classnames';
+import copy from 'copy-to-clipboard';
+import { CopyButton } from 'carbon-components-react';
 
 const ColorPaletteColor = ({
   index,
@@ -10,13 +12,26 @@ const ColorPaletteColor = ({
   isSequential,
   continuous,
 }) => {
+  const [hover, setHover] = useState(false);
+
   // determine styles
-  const sequentialStyle = !continuous ? { background: `#${hex}` } : null;
   const defaultStyle = { background: `#${hex}` };
+  const sequentialStyle = !continuous ? defaultStyle : null;
+  const copyStyle = hover
+    ? { background: `#${hex}`, filter: `brightness(125%)` }
+    : { background: `#${hex}` };
 
   // determine number
   const checkDigit = index >= 9 ? `${index + 1}. ` : `0${index + 1}. `;
   const number = isNumbered ? checkDigit : null;
+
+  const handleCopy = () => {
+    copy(hex, { format: 'text/plain' });
+  };
+
+  const handleHover = () => {
+    setHover(!hover);
+  };
 
   return (
     <div
@@ -28,7 +43,19 @@ const ColorPaletteColor = ({
         {number}
         {name}
       </span>
-      <span>{hex}</span>
+      <div>
+        <span>{hex}</span>
+        <CopyButton
+          style={isSequential ? sequentialStyle : copyStyle}
+          feedback="Copied!"
+          feedbackTimeout={3000}
+          iconDescription="Copy to clipboard"
+          onClick={handleCopy}
+          onMouseEnter={handleHover}
+          onMouseLeave={handleHover}
+          className={cx('palette-copy', { 'light-copy': lightText })}
+        />
+      </div>
     </div>
   );
 };
