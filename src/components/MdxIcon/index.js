6df3a0a167821546bdf85ccb2f5199345180ec7e
Alison Joseph <alisonejoseph@gmail.com>
2020-07-29 12:09:42 -0500

    docs(accordion): update code tab

--- src/components/MdxIcon/index.js
+++ src/components/MdxIcon/index.js
@@ -8,6 +8,9 @@ import ase from './icons/ase.png';
 import codesandbox from './icons/codesandbox.svg';
 import codepen from './icons/codepen.svg';
 import illustrator from './icons/illustrator.svg';
+import react from './icons/react.svg';
+import vue from './icons/vue.svg';
+import angular from './icons/angular.svg';
 
 const localIcons = {
   sketch,
@@ -15,6 +18,9 @@ const localIcons = {
   codesandbox,
   codepen,
   illustrator,
+  react,
+  vue,
+  angular
 };
 
 const carbonIcons = {
