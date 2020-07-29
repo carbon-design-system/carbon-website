d14b2d052d58eca4f339c8dc6a85e0fa8cee0aed
Josh Black <josh@josh.black>
2020-07-29 12:49:14 -0500

    feat(gatsby-theme-carbon): shadow nav items to work with flags

---
+++ src/gatsby-theme-carbon/util/NavItems.js
@@ -0,0 +1,42 @@
+import { useStaticQuery, graphql } from "gatsby";
+import { useNavItems as useThemeNavItems } from "gatsby-theme-carbon/src/util/NavItems";
+
+const FEATURE_FLAG_QUERY = graphql`
+  query featureFlagQuery {
+    allFeatureFlag {
+      edges {
+        node {
+          name
+          value
+        }
+      }
+    }
+  }
+`;
+
+const featureFlagsForPages = new Map([
+  ["/community/component-index", "enable-component-index-page"],
+]);
+
+export function useNavItems() {
+  const { allFeatureFlag: featureFlags } = useStaticQuery(FEATURE_FLAG_QUERY);
+  const navItems = useThemeNavItems();
+
+  return navItems.map((section) => {
+    return {
+      ...section,
+      pages: section.pages.filter((page) => {
+        if (featureFlagsForPages.has(page.path)) {
+          const flag = featureFlagsForPages.get(page.path);
+          const edge = featureFlags.edges.find(({ node }) => {
+            return node.name === flag;
+          });
+          return edge.node.value;
+        }
+        return true;
+      }),
+    };
+  });
+
+  return navItems;
+}
