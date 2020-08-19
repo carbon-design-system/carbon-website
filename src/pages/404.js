29d25a9f59c4b77c522e3a2e15d9e385f132c738
jillianhowarth <jillianlovesdogs@gmail.com>
2020-08-19 17:41:06 -0500

    docs: broken link fixes part 3

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,8 +4,8 @@ import { FourOhFour } from 'gatsby-theme-carbon';
 const links = [
   { href: '/get-started/about-carbon', text: 'About Carbon' },
   { href: '/components/overview', text: 'Components overview' },
-  { href: '/get-started/develop/react', text: 'Get started: develop' },
-  { href: '/get-started/design', text: 'Get started: design' },
+  { href: '/developing/get-started', text: 'Get started: develop' },
+  { href: '/designing/get-started', text: 'Get started: design' },
 ];
 
 const Custom404 = () => <FourOhFour links={links} />;
