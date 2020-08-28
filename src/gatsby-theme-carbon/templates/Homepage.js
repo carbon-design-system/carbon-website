cca1a0f8239f93b4d10c82427e08af539f997323
Alison Joseph <alisonejoseph@gmail.com>
2020-08-28 09:41:07 -0500

    Update events -> meetups

--- src/gatsby-theme-carbon/templates/Homepage.js
+++ src/gatsby-theme-carbon/templates/Homepage.js
@@ -47,8 +47,8 @@ const customProps = {
             <div className="bx--col-lg-4 bx--col-md-4 bx--col-sm-2 bx--offset-lg-8 bx--offset-md-4 bx--offset-sm-2 homepage--tile-header">
               <ResourceCard
                 className={styles.callToAction}
-                title="Carbon events"
-                href="/whats-happening/events"
+                title="Carbon meetups"
+                href="/whats-happening/meetups"
                 color="dark"
                 actionIcon="arrowRight"
               />
