0313228336b6a74341f8c16712c815ebb41b01d4
Alison Joseph <alisonejoseph@gmail.com>
2020-09-18 09:21:50 -0500

    feat: remove link from profile component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,38 +3,28 @@ import PropTypes from 'prop-types';
 import { settings } from 'carbon-components';
 import {
   profileContainer,
-  anchorContainer,
   cardContainer,
   contentContainer,
   imageContainer,
   nameStyle,
   titleStyle,
-  buStyle,
 } from './profile.module.scss';
 
 const { prefix } = settings;
 
-const Profile = ({ name, bu, title, serial, children, ...rest }) => (
+const Profile = ({ name, title, children, ...rest }) => (
   <div
     className={`${profileContainer} bx--col-md-4 bx--col-lg-4 bx--no-gutter-sm`}
   >
     <div className={`${prefix}--aspect-ratio ${prefix}--aspect-ratio--2x1`}>
       <div className={`${prefix}--aspect-ratio--object`}>
-        <a
-          href={`https://w3.ibm.com/bluepages/profile.html?uid=${serial}`}
-          className={anchorContainer}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           <div className={cardContainer}>
             <div className={contentContainer}>
               <div className={nameStyle}>{name}</div>
               <div className={titleStyle}>{title}</div>
-              <div className={buStyle}>{bu}</div>
             </div>
             <div className={imageContainer}>{children}</div>
           </div>
-        </a>
       </div>
     </div>
   </div>
@@ -42,9 +32,7 @@ const Profile = ({ name, bu, title, serial, children, ...rest }) => (
 
 Profile.propTypes = {
   name: PropTypes.string,
-  bu: PropTypes.string,
   title: PropTypes.string,
-  serial: PropTypes.string,
   children: PropTypes.node,
 };
 
