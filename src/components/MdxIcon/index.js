6fbe754f8e2969bc773ea9f355463dfe24ceceb0
Jeff Chew <jeff.chew@gmail.com>
2021-04-02 16:26:36 -0400

    fix(mdxicon): adding webcomponents as an icon

--- src/components/MdxIcon/index.js
+++ src/components/MdxIcon/index.js
@@ -11,6 +11,7 @@ import illustrator from './icons/illustrator.svg';
 import react from './icons/react.png';
 import vue from './icons/vue.png';
 import angular from './icons/angular.png';
+import webcomponents from './icons/webcomponents.png';
 import npm from './icons/npm.svg';
 
 const localIcons = {
@@ -22,6 +23,7 @@ const localIcons = {
   react,
   vue,
   angular,
+  webcomponents,
   npm,
 };
 
