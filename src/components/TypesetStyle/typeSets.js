e8f685fa8a32505ab37a775bd878e4e89abca00f
Scott Strubberg <sstrubberg@us.ibm.com>
2021-07-13 15:18:12 -0500

    feat(typeset): added new small and body type

---
+++ src/components/TypesetStyle/typeSets.js
@@ -0,0 +1,175 @@
+export default {
+  smallStyle: [
+    {
+      description:
+        'This is for inline code snippets and smaller code elements.',
+      version: 'mono',
+      key: 'code-01',
+      name: 'code-01',
+    },
+    {
+      description: 'This is for large code snippets and larger code elements.',
+      version: 'mono',
+      key: 'code-02',
+      name: 'code-02',
+    },
+    {
+      description:
+        'This is a multipurpose type style that can be used for field labels in components, error messages, and captions. It should not be used for body copy.',
+      key: 'label-01',
+      name: 'label-01',
+    },
+    {
+      description:
+        'This is a multipurpose type style that can be used for field labels in components, error messages, and captions. It should not be used for body copy.',
+      key: 'label-02',
+      name: 'label-02',
+    },
+    {
+      description:
+        'The legal-01 type style is for legal copy appearing in product pages.',
+      key: 'legal-01',
+      name: 'legal-01',
+    },
+    {
+      description:
+        'The legal-02 type style is for legal copy appearing in product pages.',
+      key: 'legal-02',
+      name: 'legal-02',
+    },
+  ],
+  body: [
+    {
+      description:
+        'This is for short paragraphs with no more than four lines and is commonly used in components.',
+      key: 'body-compact-01',
+      name: 'body-compact-01',
+    },
+    {
+      description:
+        'This is for short paragraphs with no more than four lines and is commonly used in the expressive type theme for layouts.',
+      key: 'body-compact-02',
+      name: 'body-compact-02',
+    },
+    {
+      description:
+        'This is commonly used in both the expressive and the productive type theme layouts for long paragraphs with more than four lines. It is a good size for comfortable, long-form reading. Use this for longer body copy in components such as accordion or structured list. Always left-align this type; never center it.',
+      key: 'body-01',
+      name: 'body-01',
+    },
+    {
+      description:
+        'This is commonly used in the expressive type theme layouts for long paragraphs with more than four lines. The looser line height and larger size makes for comfortable, long-form reading, in mediums that allow for more space. This size type is rarely used for body copy in components. Always left-align type; never center it.',
+      key: 'body-02',
+      name: 'body-02',
+    },
+  ],
+  fixedHeadings: [
+    {
+      description: 'This is for component and layout headings.',
+      key: 'productive-heading-01',
+      name: 'productive-heading-01',
+    },
+    {
+      description: 'This is for component and layout headings.',
+      key: 'productive-heading-02',
+      name: 'productive-heading-02',
+    },
+    {
+      description: 'This is for component and layout headings.',
+      key: 'productive-heading-03',
+      name: 'productive-heading-03',
+    },
+    {
+      description: 'This is for layout headings.',
+      key: 'productive-heading-04',
+      name: 'productive-heading-04',
+    },
+    {
+      description: 'This is for layout headings.',
+      key: 'productive-heading-05',
+      name: 'productive-heading-05',
+    },
+    {
+      description: 'This is for layout headings.',
+      key: 'productive-heading-06',
+      name: 'productive-heading-06',
+    },
+    {
+      description: 'This is for layout headings.',
+      key: 'productive-heading-07',
+      name: 'productive-heading-07',
+    },
+  ],
+  headings: [
+    {
+      description: 'This is for component and layout headings.',
+      key: 'expressive-heading-01',
+      name: 'expressive-heading-01',
+    },
+    {
+      description: 'This is for component and layout headings.',
+      key: 'expressive-heading-02',
+      name: 'expressive-heading-02',
+    },
+    {
+      description: 'This is for component and layout headings.',
+      key: 'expressive-heading-03',
+      name: 'expressive-heading-03',
+    },
+    {
+      description: 'Heading style',
+      key: 'expressive-heading-04',
+      name: 'expressive-heading-04',
+    },
+    {
+      description: 'Heading style',
+      key: 'expressive-heading-05',
+      name: 'expressive-heading-05',
+    },
+    {
+      description: 'Heading style',
+      key: 'expressive-heading-06',
+      name: 'expressive-heading-06',
+    },
+  ],
+  paragraphsAndQuotes: [
+    {
+      description: 'Paragraph',
+      key: 'expressive-paragraph-01',
+      name: 'expressive-paragraph-01',
+    },
+    {
+      description: '“Quote.”',
+      key: 'quotation-01',
+      name: 'quotation-01',
+    },
+    {
+      description: '“Quote.”',
+      key: 'quotation-02',
+      name: 'quotation-02',
+    },
+  ],
+  display: [
+    {
+      description: 'Display',
+      key: 'display-01',
+      name: 'display-01',
+    },
+    {
+      description: 'Display',
+      key: 'display-02',
+      name: 'display-02',
+    },
+    {
+      description: 'Display',
+      key: 'display-03',
+      name: 'display-03',
+    },
+    {
+      description: 'Display',
+      key: 'display-04',
+      name: 'display-04',
+    },
+  ],
+};
