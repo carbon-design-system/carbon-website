5a3d3cc07ab0cd758aefb5e115b473a3e03430ab
Scott Strubberg <sstrubberg@us.ibm.com>
2021-07-22 15:36:38 -0500

    fix(migration): updated links

--- src/gatsby-theme-carbon/templates/Homepage.js
+++ src/gatsby-theme-carbon/templates/Homepage.js
@@ -48,7 +48,7 @@ const customProps = {
                 className={styles.callToAction}
                 subTitle="Coming soon"
                 title="Carbon v11"
-                href="/whats-happening/v11-release/"
+                href="/releases/v11/overview"
                 color="dark"
                 actionIcon="arrowRight"
               />
