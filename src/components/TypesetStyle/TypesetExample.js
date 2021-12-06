1dec7160fc0aaf9673e6d271224f027a96edaaac
Alison Joseph <alison.joseph@us.ibm.com>
2021-12-06 11:56:54 -0600

    chore: revert v11updates to type docs and usage docs

--- src/components/TypesetStyle/TypesetExample.js
+++ src/components/TypesetStyle/TypesetExample.js
@@ -152,6 +152,8 @@ const TypesetExample = (props) => (
                 </span>
                 <br />
                 Letter spacing: {displaySpecs.letterSpacing}px
+                <br />
+                Type set: {displaySpecs.typeSet}
                 {displaySpecs.warning != null ? (
                   <span>
                     <br />
