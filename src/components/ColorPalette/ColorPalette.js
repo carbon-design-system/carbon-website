ec3856f0fbe1cb9b9e34d7bcfceac5ac4fb177e4
Taylor Jones <taylor.jones826@gmail.com>
2022-03-28 17:04:40 -0500

    fix(colorpalette): use proper dropdown size

--- src/components/ColorPalette/ColorPalette.js
+++ src/components/ColorPalette/ColorPalette.js
@@ -187,7 +187,7 @@ const ColorPalette = ({
             <Dropdown
               label="Color group selection"
               id="color-group-dropdown"
-              size="lg"
+              size="md"
               items={dropdownItems}
               onChange={onDropdownChange}
               selectedItem={dropdownItems[groupNumber - 1]}
