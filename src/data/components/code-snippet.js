f0b914160e3b9137b67803f7a525cfc0825f09ae
Alessandra Davila <adavila91@gmail.com>
2022-06-15 12:17:34 -0500

    docs(code-snippet): update example

--- src/data/components/code-snippet.js
+++ src/data/components/code-snippet.js
@@ -1,13 +1,21 @@
-export const codeSnippet = `.color-grid {
-  display: grid;
-  grid-template-columns: repeat(6, 1fr);
-}
-
-.color-grid__swatch {
-  content: '';
-  display: block;
-  height: 0;
-  padding-bottom: 100%;
+export const codeSnippet = `"scripts": {
+  "build": "lerna run build --stream --prefix --npm-client yarn",
+  "ci-check": "carbon-cli ci-check",
+  "clean": "lerna run clean && lerna clean --yes && rimraf node_modules",
+  "doctoc": "doctoc --title '## Table of Contents'",
+  "format": "prettier --write '**/*.{js,md,scss,ts}' '!**/{build,es,lib,storybook,ts,umd}/**'",
+  "format:diff": "prettier --list-different '**/*.{js,md,scss,ts}' '!**/{build,es,lib,storybook,ts,umd}/**' '!packages/components/**'",
+  "lint": "eslint actions config codemods packages",
+  "lint:styles": "stylelint '**/*.{css,scss}' --report-needless-disables --report-invalid-scope-disables",
+  "sync": "carbon-cli sync",
+  "test": "cross-env BABEL_ENV=test jest",
+  "test:e2e": "cross-env BABEL_ENV=test jest --testPathPattern=e2e --testPathIgnorePatterns='examples,/packages/components/,/packages/react/'"
+},
+"resolutions": {
+  "react": "~16.9.0",
+  "react-dom": "~16.9.0",
+  "react-is": "~16.9.0",
+  "react-test-renderer": "~16.9.0"
 }
 `;
 
