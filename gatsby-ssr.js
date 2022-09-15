7616538c2984e424e1c4378f0da0ec46c19fd672
Taylor Jones <taylor.jones826@gmail.com>
2022-09-15 11:02:46 -0500

    fix(common): remove defer

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -73,7 +73,7 @@ export const onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
   setPostBodyComponents([
     <script key="digital-data" dangerouslySetInnerHTML={{ __html: script }} />,
     <script
-      defer
+      async="async"
       key="core-metrics"
       src="https://1.www.s81c.com/common/stats/ibm-common.js"
       type="text/javascript"
