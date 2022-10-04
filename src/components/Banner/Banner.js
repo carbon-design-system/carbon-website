4bf1d5756baceb1eddfd8155ced58f58bd912cec
Alison Joseph <alison.joseph@us.ibm.com>
2022-10-04 12:27:58 -0500

    feat: add crosslink banner to carbon platform on homepage

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Button } from '@carbon/react';
+import { ArrowRight } from '@carbon/react/icons';
+
+import {
+  banner,
+  bannerText,
+  bannerHeading,
+  bannerDetails,
+  buttonBanner,
+} from './banner.module.scss';
+
+const Banner = () => {
+  return (
+    <div aria-label="banner" className={banner}>
+      <p className={bannerText}>
+        <strong className={bannerHeading}>
+          A new web experience is coming!
+        </strong>
+        <span className={bannerDetails}>
+          The v1 Carbon platform v1 is now available in public beta.
+        </span>
+      </p>
+      <Button
+        href="https://www.carbondesignsystem.com"
+        className={buttonBanner}
+        kind="ghost"
+        renderIcon={ArrowRight}>
+        Try the Carbon Platform
+      </Button>
+    </div>
+  );
+};
+
+export default Banner;
