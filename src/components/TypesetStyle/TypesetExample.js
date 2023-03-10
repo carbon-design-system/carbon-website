e17c84fe42e9a98f56acb5e095d1c65a973fa779
Jan Hassel <Jan.Hassel@ibm.com>
2023-03-10 17:15:10 +0100

    fix: fix indentation

--- src/components/TypesetStyle/TypesetExample.js
+++ src/components/TypesetStyle/TypesetExample.js
@@ -125,14 +125,14 @@ const TypesetExample = (props) => (
           style={{ padding: 0 }}
           className={`cds--typeset-example`}>
           <div className={`cds--typeset-example-row cds--row`}>
-              <div className={`cds--typeset-example-description cds--col-md-5`}>
-                <p className={versionClassNames} style={specs}>
-                  {type.description}
-                </p>
-              </div>
-              <div
-                className={`cds--typeset-example-specs cds--col-md-3 cds--padding`}>
-                <Layer>
+            <div className={`cds--typeset-example-description cds--col-md-5`}>
+              <p className={versionClassNames} style={specs}>
+                {type.description}
+              </p>
+            </div>
+            <div
+              className={`cds--typeset-example-specs cds--col-md-3 cds--padding`}>
+              <Layer>
                 <span className={`cds--type-body-short-01`}>
                   <span className={`cds--type-semibold`}>{type.name} </span>
                   <br />
