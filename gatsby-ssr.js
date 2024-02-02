f207f8b70ffe58a4010a6b10a704c3ea37d18a0e
Alison Joseph <alison.joseph@us.ibm.com>
2024-02-02 09:31:27 -0600

    chore: add common js for compliance

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import ogimage from './src/images/ogimage.png';
 
-export const onRenderBody = ({ setHeadComponents }) => {
+export const onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
+  const script = `
+  if(!window) window = {};
+  window.idaPageIsSPA = false;
+  window.digitalData = {
+    page: {
+      category: {
+        primaryCategory: 'DESIGN',
+      },
+      pageInfo: {
+        ibm: {
+          siteID: 'CARBON_DESIGN_SYSTEM_WWW_v10',
+          country: 'US',
+          industry: 'Design',
+          owner: 'carbon@us.ibm.com',
+        },
+      },
+    },
+  }`;
+
   setHeadComponents([
     <meta key="og:image" property="og:image" content={ogimage} />,
     <meta
@@ -32,6 +51,16 @@ export const onRenderBody = ({ setHeadComponents }) => {
       site="VMSGXQRS"
       key="fathom"
       defer
+    />,
+  ]);
+
+  setPostBodyComponents([
+    <script key="digital-data" dangerouslySetInnerHTML={{ __html: script }} />,
+    <script
+      defer
+      key="core-metrics"
+      src="https://1.www.s81c.com/common/stats/ibm-common.js"
+      type="text/javascript"
     />,
   ]);
 };
