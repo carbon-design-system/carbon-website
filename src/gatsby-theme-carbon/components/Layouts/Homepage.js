6ca45429ff817b64f4c14d8009d61aa22d7ff3e2
Alison Joseph <alison.joseph@us.ibm.com>
2024-03-18 14:07:14 -0500

    feat: start migration

--- src/gatsby-theme-carbon/components/Layouts/Homepage.js
+++ src/gatsby-theme-carbon/components/Layouts/Homepage.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { HomepageCallout, ResourceCard } from 'gatsby-theme-carbon';
-import HomepageTemplate from 'gatsby-theme-carbon/src/templates/Homepage';
+import HomepageTemplate from 'gatsby-theme-carbon/src/components/Layouts/Homepage';
 import { blue20, purple20, gray100 } from '@carbon/elements';
 import { calloutLink, callToAction } from './Homepage.module.scss';
 // import HomepageVideo from '../../components/HomepageVideo/HomepageVideo';
-import ImageV11 from '../../components/Homepage/Carbon-V11-Banner-Image.jpg';
+import ImageV11 from '../../../components/Homepage/Carbon-V11-Banner-Image.jpg';
 
 const FirstLeftText = () => <p>Carbon Design System</p>;
 
