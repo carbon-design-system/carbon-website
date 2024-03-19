5908cda27227915043023fa7ee450325e56efb9e
Alison Joseph <alison.joseph@us.ibm.com>
2024-03-19 18:40:44 -0500

    chore: resource links

--- src/gatsby-theme-carbon/components/LeftNav/ResourceLinks.js
+++ src/gatsby-theme-carbon/components/LeftNav/ResourceLinks.js
@@ -3,20 +3,14 @@ import ResourceLinks from 'gatsby-theme-carbon/src/components/LeftNav/ResourceLi
 
 const links = [
   {
-    title: 'Github',
-    href: 'https://github.com/carbon-design-system/gatsby-theme-carbon',
-  },
-  {
-    title: 'Carbon',
-    href: 'https://www.carbondesignsystem.com',
-  },
-  {
-    title: 'Configuration guide',
-    href: '/guides/configuration',
+    title: 'GitHub',
+    href: 'https://github.com/carbon-design-system/carbon',
   },
 ];
 
 // shouldOpenNewTabs: true if outbound links should open in a new tab
-const CustomResources = () => <ResourceLinks shouldOpenNewTabs links={links} />;
+const CustomResources = () => (
+  <ResourceLinks includeDividerSpace={false} shouldOpenNewTabs links={links} />
+);
 
 export default CustomResources;
