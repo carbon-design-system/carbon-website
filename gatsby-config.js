4048c66a4c1ebc1c6e8f6b826dc6e8e866bc07ec
Alison Joseph <alison.joseph@us.ibm.com>
2024-03-19 18:37:10 -0500

    chore: quotes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,34 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
   trailingSlash: `always`,
   siteMetadata: {
-    title: "Carbon Design System",
-    siteUrl: "https://www.carbondesignsystem.com",
+    title: 'Carbon Design System',
+    siteUrl: 'https://www.carbondesignsystem.com',
     description:
-      "Carbon is the design system for IBM web and product. It is a series of individual styles, components, and guidelines used for creating unified UI.",
+      'Carbon is the design system for IBM web and product. It is a series of individual styles, components, and guidelines used for creating unified UI.',
     keywords:
-      "IBM, design, system, Carbon, design system, Bluemix, styleguide, style, guide, components, library, pattern, kit, component, cloud",
+      'IBM, design, system, Carbon, design system, Bluemix, styleguide, style, guide, components, library, pattern, kit, component, cloud',
   },
   plugins: [
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "component-index-images",
-        path: path.join(__dirname, "src/data/index"),
-        ignore: ["**/*.js", "**/*.yml", "**/.*"],
+        name: 'component-index-images',
+        path: path.join(__dirname, 'src/data/index'),
+        ignore: ['**/*.js', '**/*.yml', '**/.*'],
       },
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "chart-index-images",
-        path: path.join(__dirname, "src/data/chart-index"),
-        ignore: ["**/*.js", "**/*.yml", "**/.*"],
+        name: 'chart-index-images',
+        path: path.join(__dirname, 'src/data/chart-index'),
+        ignore: ['**/*.js', '**/*.yml', '**/.*'],
       },
     },
     {
-      resolve: "gatsby-remark-images",
+      resolve: 'gatsby-remark-images',
       options: {
         maxWidth: 1152,
         linkImagesToOriginal: false,
@@ -36,41 +36,41 @@ module.exports = {
         withWebp: false,
       },
     },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-lodash",
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-lodash',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: "Carbon Design System",
-        short_name: "Carbon",
-        icon: "src/images/favicon.svg",
-        start_url: "/",
-        display: "browser",
+        name: 'Carbon Design System',
+        short_name: 'Carbon',
+        icon: 'src/images/favicon.svg',
+        start_url: '/',
+        display: 'browser',
       },
     },
     {
-      resolve: "gatsby-theme-carbon",
+      resolve: 'gatsby-theme-carbon',
       options: {
-        mdxExtensions: [".mdx"],
+        mdxExtensions: ['.mdx'],
         isServiceWorkerEnabled: false,
-        iconPath: "./src/images/favicon.svg",
-        titleType: "prepend",
+        iconPath: './src/images/favicon.svg',
+        titleType: 'prepend',
         repository: {
-          baseUrl: "https://github.com/carbon-design-system/carbon-website",
-          subDirectory: "",
+          baseUrl: 'https://github.com/carbon-design-system/carbon-website',
+          subDirectory: '',
         },
-        mediumAccount: "carbondesign",
+        mediumAccount: 'carbondesign',
         theme: {
-          homepage: "white",
+          homepage: 'white',
         },
       },
     },
-    "gatsby-plugin-sitemap",
+    'gatsby-plugin-sitemap',
     {
-      resolve: "gatsby-plugin-chart-index",
+      resolve: 'gatsby-plugin-chart-index',
       options: {
-        directory: path.resolve(__dirname, "./src/data/chart-index"),
+        directory: path.resolve(__dirname, './src/data/chart-index'),
       },
     },
   ],
