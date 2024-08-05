5e5ec2e7dfc0ab09abb7458c781224e427f1ebab
Jeff Chew <jeff.chew@gmail.com>
2024-08-05 16:18:29 -0400

    feat(kaltura): adding kaltura video player to gatsby

---
+++ src/components/KalturaVideo/KalturaVideo.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from "prop-types";
+
+/**
+ * KalturaVideo component: A video player container for Kaltura videos.
+ *
+ * @param {string} videoid - The ID of the Kaltura video to be played.
+ * @returns {JSX.Element} JSX element containing the video player container.
+ */
+const KalturaVideo = ({ videoid }) => {
+    return (
+        <div>
+            <c4d-video-player-container video-id={videoid}></c4d-video-player-container>
+        </div>
+    );
+};
+
+/**
+ * @typedef {object} KalturaVideoProps
+ *
+ * @property {string} videoid - The ID of the Kaltura video to be played.
+ */
+KalturaVideo.propTypes = {
+    videoid: PropTypes.string,
+};
+
+export default KalturaVideo;
